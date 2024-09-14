09049311734790101e1374eb6b8111cf3b349350
kidungamaniweb@gmail.com <kidungamaniweb@gmail.com>
2024-09-14 14:29:09 +0530

    initial

---
+++ config.py
@@ -0,0 +1,35 @@
+class Config:
+    API_ID = int(28714881)
+    API_HASH = "eea78eb9fa01f4e590d5202ffc4fe989"
+    BOT_TOKEN = "6897118835:AAG3sc-Fy65nOGyFg6CLwLd1pMuwv6wnMr8"
+    USER_SESSION = ""
+    ADMINS = [7157319028]
+    TO_CHANNEL = int()
+    
+    
+class Txt:
+    START = """
+forward a last message from a channel or send last message link from a channel 
+must give admin permission in bot or join user accout if the channel was private
+"""
+    
+    STATS = """--**ʙᴏᴛ ꜱᴛᴀᴛꜱ 🤖**--
+ʙᴏᴛ ᴩɪɴɢ: {ping}
+ᴜᴩᴛɪᴍᴇ: {uptime}
+
+--**ꜱᴇʀᴠᴇʀ 📡**--
+ᴛᴏᴛᴀʟ ᴅɪꜱᴋ: {total}
+ᴜꜱᴇᴅ ᴅɪꜱᴋ: {used} ({disk_usage}%)
+ꜰʀᴇᴇ ᴅɪꜱᴋ: {free}
+ᴛᴏᴛᴀʟ ʀᴀᴍ: {t_ram}  
+ᴜꜱᴇᴅ ʀᴀᴍ: {u_ram} ({ram_usage}%)
+ꜰʀᴇᴇ ʀᴀᴍ: {f_ram}
+ᴄᴩᴜ ᴜꜱᴀɢᴇ: {cpu_usage}% 
+
+ᴜᴩʟᴏᴀᴅꜱ: {recv}  
+ᴅᴏᴡɴʟᴏᴀᴅꜱ: {sent} """
+
+    
+    
+
+    
