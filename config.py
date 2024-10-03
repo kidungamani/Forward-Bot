936fe51c47d3ec65140e12e48c3df47d7c007285
kidungamaniweb@gmail.com <kidungamaniweb@gmail.com>
2024-10-03 10:07:34 +0530

    done

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
-    API_ID = int(28714881)
-    API_HASH = "eea78eb9fa01f4e590d5202ffc4fe989"
-    BOT_TOKEN = "6897118835:AAG3sc-Fy65nOGyFg6CLwLd1pMuwv6wnMr8"
+    API_ID = int(20979224)
+    API_HASH = "b2bb1bfca1640b91f1e98a11150cb4fc"
+    BOT_TOKEN = "7661151843:AAGN7ZQfw5ZN8p2iiHBBwZ0a9vDXcMZf-og"
     USER_SESSION = ""
-    ADMINS = [7157319028]
-    TO_CHANNEL = int(-1002168699450)
+    ADMINS = [7295048990]
+    TO_CHANNEL = int(-1002460882273)
     
     
 class Txt:
